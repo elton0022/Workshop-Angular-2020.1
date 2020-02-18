33859e0ef3bb44a16ebf420e6ac9026d9a48bc55
elton0022 <eltonrodrigues0022.er@gmail.com>
2020-02-17 21:53:49 -0300

    3 files modified, 1 added

--- src/app/components/lista/lista.component.ts
+++ src/app/components/lista/lista.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ControladorService } from '../../services/controlador.service';
 
 @Component({
   selector: 'app-lista',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaComponent implements OnInit {
 
-  constructor() { }
+  devs = [];
+
+  constructor(public controlador: ControladorService) { }
 
   ngOnInit() {
+    this.devs = this.controlador.Devs;
+  }
+
+  apagarDev(id:number){
+    this.controlador.removerDev(id);
+    this.atualizarDevs();
+  }
+
+  atualizarDevs(){
+    this.devs = this.controlador.Devs;
   }
 
 }
