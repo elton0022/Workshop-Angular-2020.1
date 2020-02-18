b3b53622a0275e02585785ff0cbcdd43cef35a3e
elton0022 <eltonrodrigues0022.er@gmail.com>
2020-02-17 21:32:08 -0300

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { ControladorService } from './services/controlador.service';
+import { ListaComponent } from './components/lista/lista.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule],
-  declarations: [ AppComponent, CabecalhoComponent],
+  declarations: [ AppComponent, CabecalhoComponent, ListaComponent],
   bootstrap:    [ AppComponent ],
   providers: [ControladorService]
 })
